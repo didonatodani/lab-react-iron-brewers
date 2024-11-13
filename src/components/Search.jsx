60f8e82f254060436d80f6ad50d369c0b9898cf2
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-11-13 15:19:05 +0100

    iterations and bonus done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,21 @@
-function Search() {
+import axios from "axios";
+import { useState } from "react";
+
+//const API_URL = "https://ih-beers-api2.herokuapp.com/beers/search?q=";
+
+function Search({ setBeers }) {
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  axios
+    .get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${search}`)
+    .then((resp) => {
+      setBeers(resp.data);
+    })
+    .catch((error) => {
+      console.log("There's been an error searching for beers:", error);
+    });
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +27,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={search}
+          onChange={handleSearch}
         />
       </div>
     </div>
--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import beersJSON from "./../assets/beers.json";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+//import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
-
+//const API_URL = "http://localhost:5173/beers/"
 function BeerDetailsPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [beer, setBeer] = useState(beersJSON[0]);
+  const [beer, setBeer] = useState();
+  const { beerId } = useParams();
+  //console.log(beerId) //all good
 
-  // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
-
-
-  // TASKS:
-  // 1. Get the beer ID from the URL, using the useParams hook.
-  // 2. Set up an effect hook to make a request for the beer info from the Beers API.
-  // 3. Use axios to make a HTTP request.
-  // 4. Use the response data from the Beers API to update the state variable.
-
-
+  function requestBeerInfo() {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`)
+      .then((resp) => {
+        setBeer(resp.data);
+      })
+      .catch((error) => {
+        console.log("There's been an error fetching one beer's data", error);
+      });
+  }
+  //console.log(API_URL + beerId) OK
+  
+  useEffect(() => {
+    requestBeerInfo();
+  }, []);
 
   // Structure and the content of the page showing the beer details. You can leave this as it is:
   return (
